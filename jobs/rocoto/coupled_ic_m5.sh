070c866f7eef1f15ff21553be196a43f6e3f7656
Shan.Sun <shan.sun@noaa.gov>
2021-09-17 16:14:13 +0000

    Creating a branch using aerosol climatology to replace GSLChem module

--- jobs/rocoto/coupled_ic_m5.sh
+++ jobs/rocoto/coupled_ic_m5.sh
@@ -54,10 +54,9 @@ fi
 # Setup ATM initial condition files
 ##cp -r $ORIGIN_ROOT/$CPL_ATMIC/$CDATE/$CDUMP/*  $ICSDIR/$CDATE/atmos/
 ##ssun "replaced by l64 data" 
-#/bin/cp -r /scratch2/BMC/gsd-hpcs/Shan.Sun/p6l64_cfs2fv3ic/$CDATE/$CDUMP/* $ICSDIR/$CDATE/atmos/
- /bin/cp -r /scratch2/BMC/gsd-hpcs/Shan.Sun/p6l64_cfs2fv3ic_m5/$CDATE/$CDUMP/* $ICSDIR/$CDATE/atmos/
+##/bin/cp -r /scratch2/BMC/gsd-hpcs/Shan.Sun/p6l64_cfs2fv3ic/$CDATE/$CDUMP/* $ICSDIR/$CDATE/atmos/
 ##ssun "replaced by l64 data with chem" 
-##/bin/cp -r /scratch2/BMC/gsd-hpcs/Shan.Sun/p6l64_cfs2fv3chemic/$CDATE/$CDUMP/* $ICSDIR/$CDATE/atmos/
+/bin/cp -r /scratch2/BMC/gsd-hpcs/Shan.Sun/p6l64_cfs2fv3chemic_m5/$CDATE/$CDUMP/* $ICSDIR/$CDATE/atmos/
 
 /bin/cp /home/Shan.Sun/helpme/gfs_ctrl_64.nc $ICSDIR/$CDATE/atmos/C384/INPUT/gfs_ctrl.nc
 ##ssun:"replaced by l127 data" 
